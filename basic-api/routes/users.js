b20506e6abbdfa950695da727dd9af266c524f7a
Luyue28 <zhan0067@hz.nl>
2024-09-25 17:54:46 +0200

    create the GET /users/:id endpoint

--- basic-api/routes/users.js
+++ basic-api/routes/users.js
@@ -7,12 +7,12 @@ const users = [
     {
         first_name: 'John',
         last_name: 'Doe',
-        email: 'johndoe@example.com',
+        email: 'johndoe@example.com'
     },
     {
         first_name: 'Alice',
         last_name: 'Smith',
-        email: 'alicesmith@example.com',
+        email: 'alicesmith@example.com'
     },
 ];
 
@@ -28,5 +28,16 @@ router.post('/', (req, res) => {
 
     res.send(`${user.first_name} has been added to the Database`);
 })
+
+// The :id part is a route parameter, which allows us to capture a dynamic value from the URL.
+router.get('/:id', (req, res) => {
+    // destructure id from req.params to extract the user ID directly from the URL
+    const { id } = req.params; 
+
+    // used the .find() method to search the data for a user whose ID matches the one captured from the URL
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+});
 
 export default router
